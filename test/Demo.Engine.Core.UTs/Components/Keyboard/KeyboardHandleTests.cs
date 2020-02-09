963dd87e8e5764864d2a178661438b4c9b1fc4ab
Michał Dembski <DemoBytom@users.noreply.github.com>
2020-02-09 13:42:53 +0100

    Added some tests to check if code coverage is properly uploaded and calculated
    ..and because tests are good, and there should be more of them ;)

---
+++ test/Demo.Engine.Core.UTs/Components/Keyboard/KeyboardHandleTests.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Linq;
+using Demo.Engine.Core.Components.Keyboard;
+using Demo.Engine.Core.Interfaces.Components;
+using Demo.Engine.Core.Platform;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace Demo.Engine.Core.UTs.Components.Keyboard
+{
+    public class KeyboardHandleTests
+    {
+        private readonly MockRepository _mockRepository;
+
+        private readonly Mock<IKeyboardCache> _mockKeyboardCache;
+        private readonly Memory<bool> _keyboardCache;
+
+        public KeyboardHandleTests()
+        {
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+
+            _mockKeyboardCache = _mockRepository.Create<IKeyboardCache>();
+            _keyboardCache = Enumerable.Repeat(false, 256).ToArray().AsMemory();
+            _mockKeyboardCache.SetupGet(o => o.KeysPressed).Returns(_keyboardCache);
+        }
+
+        private KeyboardHandle CreateKeyboardHandle()
+        {
+            return new KeyboardHandle(
+                _mockKeyboardCache.Object);
+        }
+
+        [Fact]
+        public void GetKeyPressed_Only_One_Pressed()
+        {
+            // Arrange
+            var keyboardHandle = CreateKeyboardHandle();
+            const VirtualKeys testKey = VirtualKeys.Q;
+
+            _keyboardCache.Span[(char)testKey] = true;
+
+            // Act
+            foreach (var key in Enum.GetValues(typeof(VirtualKeys)).Cast<VirtualKeys>())
+            {
+                var result = keyboardHandle.GetKeyPressed(key);
+                result.Should().Be(key == testKey, $"{key} is {result}");
+            }
+
+            // Assert
+            _mockRepository.VerifyAll();
+        }
+
+        [Fact]
+        public void GetKeyPressed_Multiple_Keys_Pressed()
+        {
+            // Arrange
+            var keyboardHandle = CreateKeyboardHandle();
+            var testKeys = new[]{
+                VirtualKeys.Q,
+                VirtualKeys.W,
+                VirtualKeys.ShiftKey
+            };
+
+            foreach (var key in testKeys)
+            {
+                _keyboardCache.Span[(char)key] = true;
+            }
+
+            // Act
+            foreach (var key in Enum.GetValues(typeof(VirtualKeys)).Cast<VirtualKeys>())
+            {
+                var result = keyboardHandle.GetKeyPressed(key);
+                result.Should().Be(testKeys.Contains(key), $"{key} is {result}");
+            }
+
+            // Assert
+            _mockRepository.VerifyAll();
+        }
+    }
+}
