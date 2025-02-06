46108d7f267d711df43ba1d3e8e699810ee6c691
Michał Dembski <DemoBytom@users.noreply.github.com>
2025-02-06 23:50:46 +0100

    Fixed step update loop
    Added a more proper fixed step update loop. Update is triggered 60 times per second (60 UPS) while Rendering is currently unbounded

--- src/Demo.Engine.Core/Services/EngineServiceNew.cs
+++ src/Demo.Engine.Core/Services/EngineServiceNew.cs
@@ -1,9 +1,9 @@
 // Copyright © Michał Dembski and contributors.
 // Distributed under MIT license. See LICENSE file in the root for more information.
 
-using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Numerics;
+using System.Threading.Channels;
 using Demo.Engine.Core.Components.Keyboard;
 using Demo.Engine.Core.Interfaces;
 using Demo.Engine.Core.Interfaces.Platform;
@@ -38,6 +38,18 @@ internal class EngineServiceNew(
     private readonly CancellationTokenSource _loopCancellationTokenSource = new();
     private bool _disposedValue;
 
+    internal record StaThreadWork(
+        Action<IRenderingEngine> Action);
+
+    private readonly Channel<StaThreadWork> _channel = Channel.CreateBounded<StaThreadWork>(
+        new BoundedChannelOptions(10)
+        {
+            AllowSynchronousContinuations = false,
+            FullMode = BoundedChannelFullMode.Wait,
+            SingleReader = true,
+            SingleWriter = false,
+        });
+
     protected override async Task DoAsync(
         IRenderingEngine renderingEngine,
         CancellationToken cancellationToken)
@@ -56,25 +68,48 @@ internal class EngineServiceNew(
             _sp!.GetRequiredService<ICube>(),
         ];
 
-        _staThreadActions.Add(
-            re => re.CreateSurface(),
-            cancellationToken);
+        await _channel.Writer.WriteAsync(
+            new StaThreadWork(
+                re => re.CreateSurface()),
+            cts.Token);
 
-        _staThreadActions.Add(
-            re => re.CreateSurface(),
-            cancellationToken);
+        await _channel.Writer.WriteAsync(
+            new StaThreadWork(
+                re => re.CreateSurface()),
+            cts.Token);
+
+        var previous = Stopwatch.GetTimestamp();
+        var lag = TimeSpan.Zero;
+
+        var msPerUpdate = TimeSpan.FromSeconds(1) / 60;
 
         while (
             IsRunning
             && !cts.Token.IsCancellationRequested)
         {
+            var current = Stopwatch.GetTimestamp();
+            var elapsed = Stopwatch.GetElapsedTime(previous, current);
+            previous = current;
+            lag += elapsed;
+            //process input
+
+            while (lag >= msPerUpdate)
+            {
+                //Update
+                foreach (var renderingSurface in renderingEngine.RenderingSurfaces)
+                {
+                    await Update(
+                          renderingSurface,
+                          keyboardHandle,
+                          keyboardCharCache);
+
+                    lag -= msPerUpdate;
+                }
+            }
+
+            //Render
             foreach (var renderingSurface in renderingEngine.RenderingSurfaces)
             {
-                await Update(
-                      renderingSurface,
-                      keyboardHandle,
-                      keyboardCharCache);
-
                 _fpsTimer.Start();
                 await Render(
                     renderingEngine,
@@ -83,8 +118,6 @@ internal class EngineServiceNew(
             }
         }
     }
-
-    private readonly BlockingCollection<Action<IRenderingEngine>> _staThreadActions = [];
 
     protected override async Task STAThread(
         IRenderingEngine renderingEngine,
@@ -101,21 +134,20 @@ internal class EngineServiceNew(
             TimeSpan.FromSeconds(1) / 60);
 
         var timestamp = Stopwatch.GetTimestamp();
+
         while (await periodicTimer.WaitForNextTickAsync(cts.Token)
                 && IsRunning
                 && doEventsOk
                 && !cts.Token.IsCancellationRequested)
         {
-            _logger.LogTrace("Tick! {elapsedMS}",
-                Stopwatch.GetElapsedTime(timestamp).Milliseconds);
+            //_logger.LogTrace("Tick! {elapsedMS}",
+            //    Stopwatch.GetElapsedTime(timestamp).TotalMilliseconds);
             timestamp = Stopwatch.GetTimestamp();
 
-            while (_staThreadActions.TryTake(
-                out var staAction))
+            while (_channel.Reader.TryRead(out var staAction))
             {
-                staAction(renderingEngine);
+                staAction.Action.Invoke(renderingEngine);
             }
-
             foreach (var renderingSurface in renderingEngine.RenderingSurfaces)
             {
                 doEventsOk &= osMessageHandler.DoEvents(
@@ -224,6 +256,7 @@ internal class EngineServiceNew(
         {
             _sin = 0;
         }
+        _angleInRadians = (_angleInRadians + 0.01f) % TWO_PI;
 
         _drawables.ElementAtOrDefault(0)
             ?.Update(renderingSurface, Vector3.Zero, _angleInRadians);
@@ -246,8 +279,6 @@ internal class EngineServiceNew(
         IRenderingEngine renderingEngine,
         Guid renderingSurfaceId)
     {
-        _angleInRadians = (_angleInRadians + 0.01f) % TWO_PI;
-
         renderingEngine.Draw(
             color: new Color4(_r, _g, _b, 1.0f),
             renderingSurfaceId: renderingSurfaceId,
